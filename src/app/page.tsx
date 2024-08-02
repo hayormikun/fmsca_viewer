1ed38a50ea1919402549e5ead274eef712180594
Favour Akomolafe <45855421+hayormikun@users.noreply.github.com>
2024-08-02 20:41:48 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { Heading } from "@/components/table/heading";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col gap-5 w-[96%]">
+    <main className="flex min-h-screen flex-col gap-5 w-[96%] mx-auto">
       <Heading header="FMSCA Viewer"/>
       <UserDetails />
 
